fe778a5d3b91a909a8f4c676007e9d0cf597a6dd
Wilker Vinicios <willkervinicios@gmail.com>
2020-11-28 18:23:23 -0300

    Criando Fields e Listeners TelaPrincipal.java

--- src/br/com/senac/TelaPrincipal.java
+++ src/br/com/senac/TelaPrincipal.java
@@ -1,6 +1,30 @@
 package br.com.senac;
 
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class TelaPrincipal {
 
+    private JButton btnCliente;
+    private JList listaClientes;
+
+
+    public TelaPrincipal() {
+        btnCliente.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        listaClientes.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+
+            }
+        });
+    }
 }
