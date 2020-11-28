1120fa8334fb49d2eacafc6e181917d5bc63ae9e
Wilker Vinicios <willkervinicios@gmail.com>
2020-11-28 18:22:48 -0300

    Criando Fields e Listeners TelaCadastro.java

--- src/br/com/senac/TelaCadastro.java
+++ src/br/com/senac/TelaCadastro.java
@@ -1,4 +1,49 @@
 package br.com.senac;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class TelaCadastro {
+
+    private JButton btnAdicionar;
+    private JButton btnAtualizar;
+    private JButton btnExcluir;
+    private JTextField textNome;
+    private JTextField textCpf;
+    private JTextField textRg;
+    private JTextField textOrgaoExpeditor;
+    private JTextField textDatanascimento;
+    private JTextField textNomeRua;
+    private JTextField textTipoRua;
+    private JTextField textNumeroRua;
+    private JTextField textComplemento;
+    private JTextField textBairro;
+    private JTextField textCep;
+    private JTextField textCidade;
+    private JTextField textEstado;
+
+    public TelaCadastro() {
+
+        btnAdicionar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        btnAtualizar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        btnExcluir.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
